624acefebfcded10e5561cb8accbac30e5c3110c
Jack O'Hara <jack@jackohara.io>
2021-03-14 20:14:57 +0000

    Added conversion acceptance tests for xml conversion

--- test/DataConverter.Conversion.Tests/ConversionTests.cs
+++ test/DataConverter.Conversion.Tests/ConversionTests.cs
@@ -33,5 +33,30 @@ namespace DataConverter.Tests
             Assert.Equal(StructuredDataFormat.Json, result.Format);
             Assert.Equal(await File.ReadAllTextAsync(expectedFilePath), result.Contents);
         }
+
+        [InlineData("./input.csv", "./expected.xml")]
+        [InlineData("./multi-row.csv", "./multi-row-expected.xml")]
+        [Theory]
+        public async Task Converts_CSV_to_expected_xml(string inputFilePath, string expectedFilePath)
+        {
+            var options = new StructuredDataConversionOptions
+            {
+                InputData = new StructuredData
+                {
+                    Format = StructuredDataFormat.Csv,
+                    Contents = await File.ReadAllTextAsync(inputFilePath)
+                },
+                TargetFormat = StructuredDataFormat.Xml
+            };
+
+            var converter = new StructuredDataConverter(
+                new StructuredDataInterpreterFactory(),
+                new StructuredDataWriterFactory());
+
+            var result = converter.Convert(options);
+
+            Assert.Equal(StructuredDataFormat.Json, result.Format);
+            Assert.Equal(await File.ReadAllTextAsync(expectedFilePath), result.Contents);
+        }
     }
 }
