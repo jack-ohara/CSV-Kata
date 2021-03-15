907f8c83df41c2a3767adf9bd29b3f943974d05e
Jack O'Hara <jack@jackohara.io>
2021-03-15 19:53:58 +0000

    Created a failing unit test for null input data in the json writer

---
+++ test/DataConverter.Conversion.Tests/DataWriting/Json/JsonDataWriterTests.cs
@@ -0,0 +1,19 @@
+﻿using DataConverter.Conversion.DataWriting.Json;
+using Xunit;
+
+namespace DataConverter.Conversion.Tests.DataWriting.Json
+{
+    public class JsonDataWriterTests
+    {
+        [Fact]
+        public void Returns_empty_string_if_input_is_null()
+        {
+            var sut = new JsonDataWriter();
+
+            var result = sut.WriteData(null);
+
+            Assert.Equal(StructuredDataFormat.Json, result.Format);
+            Assert.Equal(string.Empty, result.Contents);
+        }
+    }
+}
