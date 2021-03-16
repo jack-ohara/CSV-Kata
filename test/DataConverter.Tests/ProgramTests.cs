ffb3d7f24c649d110376d480e4626130d9b057b9
Jack O'Hara <jack@jackohara.io>
2021-03-16 18:15:36 +0000

    Created a new test class for the cli project. Added a test to check it rejects unrecognised args

---
+++ test/DataConverter.Tests/ProgramTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DataConverter.Tests
+{
+    public class ProgramTests : IAsyncLifetime
+    {
+        private TextWriter _consoleOutput;
+        private StringBuilder _stringBuilder;
+
+        public async Task DisposeAsync()
+        {
+            await _consoleOutput.DisposeAsync();
+        }
+
+        public Task InitializeAsync()
+        {
+            _stringBuilder = new StringBuilder();
+            _consoleOutput = new StringWriter(_stringBuilder);
+
+            Console.SetOut(_consoleOutput);
+
+            return Task.CompletedTask;
+        }
+
+        [Fact]
+        public void Writes_an_error_to_the_console_when_an_unrecognised_argument_is_supplied()
+        {
+            var args = new[] { "--test", "hello" };
+
+            var result = Program.Main(args);
+            var consoleOutput = GetConsoleOutput();
+
+            Assert.Equal(1, result);
+            Assert.Contains("Option 'test' is unknown", consoleOutput);
+        }
+
+        private string GetConsoleOutput()
+        {
+            return _stringBuilder.ToString();
+        }
+    }
+}
