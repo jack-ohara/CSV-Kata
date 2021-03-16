656bc9834db030f53b3000b611e60496a4db8b6d
Jack O'Hara <jack@jackohara.io>
2021-03-16 18:16:12 +0000

    Installed CommandLineParser and got the test green

--- src/DataConverter/Program.cs
+++ src/DataConverter/Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
 
 namespace DataConverter
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var parser = new Parser(x => x.HelpWriter = Console.Out);
+
+            var result = parser.ParseArguments<CommandLineOptions>(args)
+                .MapResult(RunConversion, HandleErrors);
+
+            return result;
         }
+
+        private static int RunConversion(CommandLineOptions options)
+        {
+            try
+            {
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+                return 1;
+            }
+        }
+
+        private static int HandleErrors(IEnumerable<Error> errors)
+        {
+            return 1;
+        }
+    }
+
+    class CommandLineOptions
+    {
+        [Option('c', "csvInputFile", Required = true, HelpText = "Specifies the CSV input file to convert.")]
+        public string CsvInputFileName { get; set; }
+
+        [Option('f', "format", Default = "json", HelpText = "Specifies the target format to convert to.")]
+        public string TargetFormat { get; set; }
+
     }
 }
