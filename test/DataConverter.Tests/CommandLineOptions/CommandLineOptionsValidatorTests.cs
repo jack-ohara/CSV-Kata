a7abd5a3418b2e81f85a9e4ae5de9e196d6fc8bb
Jack O'Hara <jack@jackohara.io>
2021-03-16 19:05:55 +0000

    Created a new test for a fluent validator on the command line options

---
+++ test/DataConverter.Tests/CommandLineOptions/CommandLineOptionsValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿using DataConverter.CommandLineOptions;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace DataConverter.Tests.CommandLineOptions
+{
+    public class CommandLineOptionsValidatorTests
+    {
+        [InlineData(null)]
+        [InlineData("")]
+        [Theory]
+        public void Returns_an_error_when_csv_input_is_empty(string csvInput)
+        {
+            var options = new Options { CsvInputFileName = csvInput };
+
+            var sut = new CommandLineOptionsValidator();
+
+            var result = sut.TestValidate(options);
+
+            result.ShouldHaveValidationErrorFor(o => o.CsvInputFileName)
+                .WithErrorMessage("'csvInputFile' must not be empty.");
+        }
+    }
+}
